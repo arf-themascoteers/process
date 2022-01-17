f08a71cd6b88312e58ca8fa4f3613e5201c469c2
Arif <80994072+arf-themascoteers@users.noreply.github.com>
2022-01-17 16:29:10 +1100

    Done

--- cropper.py
+++ cropper.py
@@ -2,7 +2,10 @@ from rembg.bg import remove
 import numpy as np
 import io
 from PIL import Image, ImageFile
+import os
 import cv2
+
+MARKER = False
 
 def crop(name):
     input_path = f'raw/{name}'
@@ -14,23 +17,36 @@ def crop(name):
     it = iter(faces)
     x, y, w, h = next(it)
     center = (x + int(w/2), y + int(h/2))
+
     modified_width_ratio = 1.5
     modified_width = int(w * modified_width_ratio)
     width_height_ratio = 1.2
     modified_height = int(modified_width * width_height_ratio)
 
-    modified_top_left = (center[0] - int(modified_height/2), center[1] - int(modified_width/2))
-    modified_bottom_right = (modified_top_left[0] + modified_height, modified_top_left[1] + modified_width)
+    modified_top_left = (center[0] - int(modified_width/2), center[1] - int(modified_height/2))
+    if modified_top_left[0] < 0:
+        modified_top_left = (0, modified_top_left[1])
+    if modified_top_left[1] < 0:
+        modified_top_left = (modified_top_left[0], 0)
 
-    cropped_image = img[modified_top_left[0]:modified_bottom_right[0], modified_top_left[1]:modified_bottom_right[1]]
-    cropped_center = (int(cropped_image.shape[1]/2), int(cropped_image.shape[0]/2))
-    #cv2.rectangle(img,(x,y),(x+w,y+h), color=(255,0,255))
-    #cv2.rectangle(img,modified_top_left,modified_bottom_right, color=(255,255,0))
+    modified_bottom_right = (modified_top_left[0] + modified_width, modified_top_left[1] + modified_height)
 
-    #cv2.circle(img, (eye_left_x,eye_left_y), radius=10, color=(255, 0, 255), thickness=5)
-    #cv2.circle(img, (eye_right_x,eye_right_y), radius=4, color=(0, 255, 0), thickness=1)
-    print(f"Width, Height: {cropped_image.shape}")
+    if MARKER:
+        cv2.rectangle(img,(modified_top_left),(modified_bottom_right), color=(255,0,255))
+        cv2.circle(img, center, radius=2, color=(255, 0, 255), thickness=5)
+        cv2.imwrite(output_path, img)
+        exit(0)
 
-    cv2.circle(cropped_image, cropped_center, radius=2, color=(255, 0, 255), thickness=5)
+    cropped_image = img[modified_top_left[1]:modified_bottom_right[1], modified_top_left[0]:modified_bottom_right[0]]
 
     cv2.imwrite(output_path,cropped_image)
+
+def crop_all():
+    directory = "raw"
+    for file in os.listdir(directory):
+        print(f"Processing {file}")
+        crop(file)
+
+if __name__ == "__main__":
+    crop_all()
+    #crop("bakar.png")
---
+++ marking.py
@@ -0,0 +1,9 @@
+# cropped_center = (int(cropped_image.shape[1]/2), int(cropped_image.shape[0]/2))
+# cv2.rectangle(img,(x,y),(x+w,y+h), color=(255,0,255))
+# cv2.rectangle(img,modified_top_left,modified_bottom_right, color=(255,255,0))
+
+# cv2.circle(img, (eye_left_x,eye_left_y), radius=10, color=(255, 0, 255), thickness=5)
+# cv2.circle(img, (eye_right_x,eye_right_y), radius=4, color=(0, 255, 0), thickness=1)
+# print(f"Width, Height: {cropped_image.shape}")
+
+# cv2.circle(cropped_image, cropped_center, radius=2, color=(255, 0, 255), thickness=5)
